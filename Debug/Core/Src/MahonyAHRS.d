8dc6842ef0d737a9a31f335b50bf3338278db311
FaterYU <1042277145@qq.com>
2023-04-08 16:46:43 +0800

    gitignore cache

---
+++ Debug/Core/Src/MahonyAHRS.d
@@ -0,0 +1,2 @@
+Core/Src/MahonyAHRS.o: ../Core/Src/MahonyAHRS.c ../Core/Inc/MahonyAHRS.h
+../Core/Inc/MahonyAHRS.h:
---
+++ Debug/Core/Src/SolveTrajectory.d
@@ -0,0 +1,3 @@
+Core/Src/SolveTrajectory.o: ../Core/Src/SolveTrajectory.c \
+ ../Core/Inc/SolveTrajectory.h
+../Core/Inc/SolveTrajectory.h:
